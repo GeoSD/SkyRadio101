afc64b22d790776dbcd47fbb256efef680a929c2
Georgy Dyagilev <g.dyagilev@gmail.com>
2019-01-19 15:54:11 +0700

    Some refactoring.

--- SkyRadio101/AppDelegate.swift
+++ SkyRadio101/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,16 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        let audioSession = AVAudioSession.sharedInstance()
-        
-        do {
-            try audioSession.setCategory(.playback, mode: .default, options: [.mixWithOthers, .allowAirPlay])
-            try audioSession.setActive(true)
-        } catch {
-            print("Setting category to AVAudioSessionCategoryPlayback failed.")
-        }
-        
         return true
     }
 
@@ -38,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
--- SkyRadio101/ViewController.swift
+++ SkyRadio101/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import MediaPlayer
 import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var playpauseButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
     
     let stringURL = "https://www.skyradio.nl/player/skyradio.pls"
     
@@ -21,24 +23,65 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let url = URL(string: stringURL) else {
-            print("Error while convert URL")
-            return
-        }
-        
-        let audioURLAsset = AVURLAsset(url: url)
-        playerItem = AVPlayerItem(asset: audioURLAsset)
-        player = AVPlayer(playerItem: playerItem)
+        setupAudioSession()
+        setupPlayer()
+        setupCommandCenter()
     }
 
     @IBAction func playPauseButtonTapped(_ sender: UIButton) {
         if player.rate == 0 {
+            statusLabel.text = "Playing"
             player.play()
         } else {
+            statusLabel.text = "Paused"
             player.pause()
         }
+        
     }
+    
+    func setupPlayer() {
+        guard let url = URL(string: stringURL) else {
+            print("Error while convert URL")
+            return
+        }
+        statusLabel.text = "Paused"
+
+        player = AVPlayer(url: url)
+    }
+    
+    func setupAudioSession() {
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [.allowAirPlay, .allowBluetooth, .allowBluetoothA2DP, .mixWithOthers])
+            try audioSession.setActive(true)
+            UIApplication.shared.beginReceivingRemoteControlEvents()
+            print("do block works fine.")
+        } catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.\(error)")
+        }
+    }
+    
+    func setupCommandCenter() {
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = [MPMediaItemPropertyTitle: "SkyRadio 101"]
+        
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.isEnabled = true
+        commandCenter.pauseCommand.isEnabled = true
+        
+        commandCenter.playCommand.addTarget { (event) -> MPRemoteCommandHandlerStatus in
+            self.player.play()
+            self.statusLabel.text = "Playing"
+            return .success
+        }
+        
+        commandCenter.pauseCommand.addTarget { (event) -> MPRemoteCommandHandlerStatus in
+            self.player.pause()
+            self.statusLabel.text = "Paused"
+            return .success
+        }
+    }
+    
 }
 
 
